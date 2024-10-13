545d8bf43f3082a91d7ded390f358818067b748f
Robert Pliszka <robplisz@gmail.com>
2024-10-12 21:04:48 -0400

    Fetched data from the API url and storing in the tours state

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,27 @@
+import { useState, useEffect } from 'react';
+
 const url = 'https://www.course-api.com/react-tours-project';
 
 const App = () => {
-  return <h2>Tours Starter</h2>;
+  // State weather the data is loading
+  const [isLoading, setIsLoading] = useState(false);
+  // State which will hold data fetched from API
+  const [tours, setTours] = useState(null);
+
+  useEffect(() => {
+    const fetchTours = async () => {
+      try {
+        const resp = await fetch(url);
+        const tour = await resp.json();
+
+        setTours(tour);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchTours();
+  }, []);
+
+  return <div>{console.log(tours)}</div>;
 };
 export default App;
