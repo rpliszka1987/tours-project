0c5406f02851eaf7d3db249ac791b73363ffc039
Robert Pliszka <robplisz@gmail.com>
2024-10-13 11:39:16 -0400

    Created read more functionlality to the description which originally only shows 200 characters and all when clicked to read more

--- src/components/Tour.jsx
+++ src/components/Tour.jsx
@@ -1,11 +1,25 @@
+import { useState } from 'react';
+
 const Tour = ({ id, image, info, name, price, removeTour }) => {
+  // State for controlling read more.
+  const [readMore, setReadMore] = useState(false);
+
   return (
     <article className="single-tour">
       <img src={image} alt={name} className="img" />
       <span className="tour-price">${price}</span>
       <div className="tour-info">
         <h5>{name}</h5>
-        <p>{info}</p>
+        <p>
+          {readMore ? info : `${info.substring(0, 200)}...`}
+          <button
+            type="button"
+            className="info-btn"
+            onClick={() => setReadMore(!readMore)}
+          >
+            {readMore ? 'Read less' : 'Read more'}
+          </button>
+        </p>
         <button
           type="button"
           className="btn btn-block delete-btn"
