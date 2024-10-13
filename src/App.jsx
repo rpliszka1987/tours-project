ea028fc95042840f32cf3a9fdcc8b041ff364d76
Robert Pliszka <robplisz@gmail.com>
2024-10-12 21:11:25 -0400

    Put conditions to catch errors from the API url

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,9 @@ const url = 'https://www.course-api.com/react-tours-project';
 
 const App = () => {
   // State weather the data is loading
-  const [isLoading, setIsLoading] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
+  // State for when an error occurs
+  const [isError, setIsError] = useState(false);
   // State which will hold data fetched from API
   const [tours, setTours] = useState(null);
 
@@ -12,15 +14,31 @@ const App = () => {
     const fetchTours = async () => {
       try {
         const resp = await fetch(url);
+
+        if (!resp.ok) {
+          setIsError(true);
+          setIsLoading(false);
+          return;
+        }
         const tour = await resp.json();
 
         setTours(tour);
       } catch (error) {
+        setIsError(true);
         console.log(error);
       }
+      setIsLoading(false);
     };
     fetchTours();
   }, []);
+
+  if (isLoading) {
+    return <h2>Loading data ...</h2>;
+  }
+
+  if (isError) {
+    return <h2>There was an error</h2>;
+  }
 
   return <div>{console.log(tours)}</div>;
 };
