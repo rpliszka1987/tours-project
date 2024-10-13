0f66ae10f7914e44b02764fc0736608734de6bde
Robert Pliszka <robplisz@gmail.com>
2024-10-12 21:24:18 -0400

    Added a remove tour button from each tour

--- src/components/Tour.jsx
+++ src/components/Tour.jsx
@@ -1,15 +1,20 @@
 const Tour = (props) => {
-  return <div>
-    {props.data.map((data) => {
-        const {id, name, info,image,price } = data;
-        return <div key={id}>
+  return (
+    <div>
+      {props.data.map((data) => {
+        const { id, name, info, image, price } = data;
+        return (
+          <div key={id}>
             <h2>{name}</h2>
             <p>{info}</p>
             <img src={image} alt={name} />
             <span>${price}</span>
-        </div>
-    })}
-  </div>
+            <button className="btn">Remove Tour</button>
+          </div>
+        );
+      })}
+    </div>
+  );
 };
 
 export default Tour;
